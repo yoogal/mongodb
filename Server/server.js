d94ca8d88e9a1125152f9bef1d7b0d2663aadd64
micwi <micwilson13899@gmail.com>
2019-02-05 22:01:14 +0530

    GET/todos

--- Server/server.js
+++ Server/server.js
@@ -18,6 +18,13 @@ app.post('/todos',(req,res)=>{
   });
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos })
+  },(e)=>{
+    res.send(e.message)
+  });
+});
 app.listen(3000,()=>{
   console.log('Listening in port 3000');
 })
