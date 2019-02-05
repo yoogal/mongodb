ef4646f2f031ee73a558dea8af8e66b2cd52e9ae
micwi <micwilson13899@gmail.com>
2019-02-05 22:52:07 +0530

    Get Todo by Id

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/Todo.js');
+var {ObjectId}=require('mongodb');
 
 var express=require('express');
 var bodyParser=require('body-parser');
@@ -20,9 +21,24 @@ app.post('/todos',(req,res)=>{
 
 app.get('/todos',(req,res)=>{
   Todo.find().then((todos)=>{
-    res.send({todos })
+    res.send({todos})
   },(e)=>{
     res.send(e.message)
+  });
+});
+
+app.get('/todos/:id',(req,res)=>{
+  var id=req.params.id;
+  if(!ObjectId.isValid(id)){
+    return res.status(400).send();
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+        return res.status(400).send();
+    }
+    res.send({todo});
+  },(err)=>{
+    res.send(e.message);
   });
 });
 app.listen(3000,()=>{
